89e516cbce8576aeff348a850ff96cd69eaea71a
Der Uli im Netz <admin@deruli.de>
2020-07-07 22:27:17 +0200

    Update PersonalDataQueryTest.php

--- src/content/modules/personal_data_export/tests/objects/PersonalDataQueryTest.php
+++ src/content/modules/personal_data_export/tests/objects/PersonalDataQueryTest.php
@@ -24,7 +24,6 @@ class PersonalDataQueryTest extends \PHPUnit\Framework\TestCase
         $user->setFirstname("John");
         $user->setPassword("foobar");
         $user->save();
-
         return $user;
     }
 
@@ -32,11 +31,28 @@ class PersonalDataQueryTest extends \PHPUnit\Framework\TestCase
     {
         $this->getTestUser();
         $query = new CorePersonalDataResponder();
+
         $this->assertEquals(
             "foo123@bar.de",
             $query->searchPerson("testuser2")[0]->email
         );
-                
+
+        $this->assertEquals(
+            "foo123@bar.de",
+            $query->searchPerson("foo123@bar.de")[0]->email
+        );
+
+        $this->assertEquals(
+            "foo123@bar.de",
+            $query->searchPerson("Doe")[0]->email
+        );
+
+
+        $this->assertEquals(
+            "foo123@bar.de",
+            $query->searchPerson("Doe, John")[0]->email
+        );
+
         $this->assertNull($query->searchPerson("Existiert, Nicht")[0]->email);
     }
 }
